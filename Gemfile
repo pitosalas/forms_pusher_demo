4c7a5af719cc785d3cfde8cfe02017c3b063000b
Pito Salas <pitosalas@gmail.com>
2017-10-30 21:59:30 -0400

    closer to rails pa 2 solution

--- Gemfile
+++ Gemfile
@@ -52,3 +52,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'binding_of_caller'
+gem 'awesome_print'
+gem 'better_errors'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories
+  resources :items do
+    collection do
+      get 'search'
+      get 'do_search'
+    end
+    resources :comments, only: [:index, :create]
+  end
+  root 'items#index'
+
 end
